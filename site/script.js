4d61862031eadbaae3c6258fde0c6b6f12636445
adcand <adrianocandidocontato@gmail.com>
2021-09-27 09:33:08 -1100

    added files

---
+++ site/script.js
@@ -0,0 +1,63 @@
+const getPositions = document.querySelectorAll(".position");
+const winnerText = document.querySelector(".winner");
+const positions = Array.from(getPositions);
+
+positions.forEach(area => area.addEventListener("click", addCharacter));
+
+let thereIsAWin = false;
+let positionsUsed = [];
+let winPossibilities = [
+  // Horizontal
+  ["b1", "b2", "b3"], 
+  ["b4", "b5", "b6"], 
+  ["b7", "b8", "b9"],
+
+  // Vertical
+  ["b1", "b4", "b7"], 
+  ["b2", "b5", "b8"], 
+  ["b3", "b6", "b9"],
+
+  // Diagonal
+  ["b1", "b5", "b9"], 
+  ["b3", "b5", "b7"]
+];
+
+function addCharacter(e) {
+  const emptyArea = e.target.classList.contains("circle");
+
+  if (!emptyArea && !thereIsAWin) {
+    createCircle(e);
+    positionsUsed.push(e.target.id);
+    decideWin();
+    return;
+  }
+
+  return;
+
+  function createCircle(e) {
+    e.target.classList.add("circle");
+  }
+
+  function createX(e) {
+    e.target.classList.add("x");
+  }
+}
+
+function decideWin() {
+  winPossibilities.forEach(possibility => {
+    const win = possibility.every(option => positionsUsed.includes(option));
+    
+    if (win) {
+      winnerText.textContent = "Someone win!"
+      thereIsAWin = true;
+    };
+  });
+}
+
+// 8 possible combinations you can win 
+// horizontally = area + 1 + 1
+// vertically = area + 3 + 3
+// diagnally = area + 4 + 4 or area + 2 + 2
+
+// TODO
+// . alternate the characters 
